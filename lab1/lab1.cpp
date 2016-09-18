b8764e18da69070e3cfe08df4c37fbb498dc8dfa
Jamie Hayes <jhayes011@student.butte.edu>
2016-09-18 09:50:44 +0000

    Lab 1 folder updated for ASCII art

---
+++ lab1/lab1.cpp
@@ -0,0 +1,25 @@
+// created by Jamie Hayes resubmition of ascii
+#include <iostream>
+using namespace std;
+
+int main () {
+    // ascii of R2D2 from star wars 
+    
+    cout << "       ________       " << endl;
+    cout << "      /    (0)  Y     " << endl;
+    cout << "   __|__________|__   " << endl;
+    cout << "  |  |   * * *  |  |  " << endl;
+    cout << "  |__|    R2D2  |__|  " << endl;
+    cout << "   | |    (^)   | |   " << endl;
+    cout << "   | |__________| |   " << endl;
+    cout << "  |~  `________/  ~|  " << endl;
+    cout << "  / = |  / = |  / = | " << endl;
+    cout << " (____] (____] (____] " << endl;
+    cout << endl;
+    
+    cout << " Star Wars R2D2 " << endl;
+    
+    cout << endl;
+    
+    return 0;
+}
