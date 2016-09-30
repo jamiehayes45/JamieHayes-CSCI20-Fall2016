82a7ace6443e4dce5d2f7dcf1fba26bc01933b88
Jamie Hayes <jhayes011@student.butte.edu>
2016-09-30 06:05:16 +0000

    Lab 9 folder debugged and updated

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -1,70 +1,66 @@
 // Created by Jamie Hayes
 // Created on 9/27/16 : functions and classes
 #include <iostream>
-#include <string>
 #include <cmath> 
 using namespace std;
 
 class TempratureConverter {             // class name
     public:                             // class type can be changed and accessed by user
-    void SetTempFromKelvin() const;      // accessr
-    void SetTempFromCelcius;
-    void SetTempFromFahrenheit;
-    TempertureConverter();             // default constructor
+    void SetTempFromKelvin(double kelvinTemp);  // mutator
+    void SetTempFromCelcius(double celsiusTemp);
+    void SetTempFromFahrenheit(double fahrentheitTemp);
+    void PrintTemperatures() const;
     
-    double GetTempFromKelvin;
-    double GetTempAsCelsius;
-    double GetTempAsFahrenheit;
+    TempertureConverter();  // default constructor
+    TemperatureConverter(double kelvinTemp); // overloading
+    
+    double GetTempFromKelvin() const; // accessor
+    double GetTempAsCelsius() const;
+    double GetTempAsFahrenheit() const;
     
     private: 
-        int kelvin_;
+        double kelvin_;
     }
 };
-TempratureConverter :: TempratureConverter () {     // default constructor
-    kelvin_ = "0";
-    return;
+
+TeperatureConverter :: TemperatureConverter(double kelvinTemp) {  // overloading
+    kelvin_ = kelvinTemp;
 }
 
-void TempratureConverter :: SetTempFromKelvin () {    // Accessor
-    
+TempratureConverter :: TempratureConverter () {     // default constructor
+    kelvin_ = 0.0 ;
+}
+
+void TemperatureCOnverter :: SetTempFromCelcius(double celsiusTemp) {
+    kelvin_ = celsiusTemp +273.15; // equation k = c + 273.15
+}
+
+void TempratureConverter :: SetTempFromFahrenheit(double fahrenheitTemp) {
+    kelvin_ = (5.00*(fahrenheitTemp -32.00)/9.00) + 273.15; // equation k = (5*(f-32) /9) +273.15
+}
+
+void TempratureConverter :: SetTempFromKelvin (double kelvinTemp) {   
+    kelvin_ - kelvinTemp
 } 
 
-void TempratureConverter :: SetTempFromCelcius () {    //Accesor
-    (k=c + 273.15) = Celsius_value;
-    return;
+double TempratureConverter :: GetTempFromKelvin () const {
+    return kellvin_;
 }
 
-void TempratureConverter :: SetTempFromFahrenheit (){   // Acessor
-    k=(5 *(f-32)/9) + 273.15 = Fahrenheit_value;
-    return;
+double TempertureConverter :: GetTempAsCelsius () const {       // mutator
+    return kelvin_ - 273.15; //equation  c= k - 273.15 return in order to get celsius
 }
 
-double TempratureConverter :: GetTempFromKelvin () {    // mutator
-    returns kelvin_value;
-}
-
-double TempertureConverter :: GetTempAsCelsius () {       // mutator
-    k- 273.15;
-    return CelsiusTemp;
-}
-
-double TempratureConverter :: GetTempAsFahrenheit () {    // mutatt
-    ((c*9)/5 + 32)
-    return FahrenheitTemp;
+double TempratureConverter :: GetTempAsFahrenheit ()const {    // mutatt
+    return ((kelvin_* 9) /5 + 32); // equation ((celcius * 9) divided by 5 + 32)
 }
 
 // PrintTemperatures
-item1.Print();
-item1.PrintTemperatures("kelvin_", 0);
-item1.Print();
-
-item2.Prit();
-item2.PrintTemperatures("celius");
-item2.Print();
-
-item3.Print();
-item3.PrintTemperatures("fahrenheit");
-item3.Print();
-
-return 0;
-}
+void TempratureConverter::PrintTemperatures() const {
+    cout << "kelvin value: " << GetTempFromKelvin();
+    cout << endl;
+    cout << " celsius value: " << GetTempAsCelsius();
+    cout << endl;
+    cout << "fahrenheit value: " << GetTempAsFahrenheit();
+    cout << endl;
+} 
