21d7ec626e9731493418ca81818705f93e53a52d
Jamie Hayes <jhayes011@student.butte.edu>
2016-11-03 08:04:31 +0000

    lab2 to be submitted

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,12 +1,28 @@
-// created by Jamie Hayes Algorithm number game
-#include <iostream>
-#include <cstdlib> // enables use of random
-#include <ctime> // enables use of time
-using namespace std;
+// Created by Jamie Hayes 
+// Title: Algorithm Number Game
 
-int main () {
-    // lab2 algorithm computer will generate a random number, and a user must guess it correctly to win
-    
-    
-    
-}
+/*#include <iostream> // basic stream
+#include <cstdlib>    // enables use of random
+#include <ctime>      // enables use of time
+*/ 
+
+// write up description 
+// lab2 algorithm computer will generate a random number, and a user must guess it correctly to win
+ 
+ /*set the variables comptNumber
+ * set the variable randomNumber
+ * set the variable 'score' // 10 points within score
+ 
+ * set a range for the computer to choose a random number from via 1-10
+ * set the computer to choose a random number between the range
+ * save it withn the variable comptNumber
+ * cin a users guess, and save it in randomNumber
+ * compare it to computers number using a loop
+ 
+ *if the users number is higher than the computer number tell the user lower, and deduct one point from the total score
+ *if the user number is lower than the computer number, tell the user higher, and deduct one point from the totla score
+ *if the user number is the same as the computer number, thell them they got it right, and earn a point
+ * continue for ten rounds 
+ *add total score points
+ *tell user if they won or lost depending on how many rounds they win out of ten 
+ */
